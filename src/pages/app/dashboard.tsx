632f433b16aa4656babf287ede039b9467dc8b7d
Samuel Morais030202 <moraissamuel009@gmail.com>
2025-01-26 00:40:21 -0300

    feat: configurando o helmet async

--- src/pages/app/dashboard.tsx
+++ src/pages/app/dashboard.tsx
@@ -1,6 +1,9 @@
+import { Helmet } from 'react-helmet-async'
+
 export function Dashboard() {
   return (
     <div>
+      <Helmet title="Dashboard" />
       <p>Dashboar page</p>
     </div>
   )
--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,6 +1,9 @@
+import { Helmet } from 'react-helmet-async'
+
 export function SiginIn() {
   return (
     <div>
+      <Helmet title="Login" />
       <p>Sign in page</p>
     </div>
   )
