d0d0dd1e2e28c3a1849ec8ab4d2a02fb3dd44f00
Samuel Morais030202 <moraissamuel009@gmail.com>
2025-01-26 11:32:16 -0300

    feat: configurando os themas da aplicação e adicionando dropdown no cabeçalho

---
+++ src/components/account-menu.tsx
@@ -0,0 +1,45 @@
+import { Building, ChevronDown, LogOut } from 'lucide-react'
+
+import { Button } from './ui/button'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from './ui/dropdown-menu'
+
+export function AccountMenu() {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button
+          variant="outline"
+          className="selection:none flex items-center gap-2"
+        >
+          Pizza Shop
+          <ChevronDown />
+        </Button>
+      </DropdownMenuTrigger>
+
+      <DropdownMenuContent align="end" className="w-56">
+        <DropdownMenuLabel className="flex flex-col">
+          <span>Samuel</span>
+          <span className="textxs font-normal text-muted-foreground">
+            moraissamuel009@gmail.com
+          </span>
+        </DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>
+          <Building className="mr-2 size-4" />
+          <span>Perfil da loja</span>
+        </DropdownMenuItem>
+        <DropdownMenuItem className="text-rose-500 dark:text-rose-400">
+          <LogOut className="mr-2 size-4" />
+          <span>Sair</span>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
