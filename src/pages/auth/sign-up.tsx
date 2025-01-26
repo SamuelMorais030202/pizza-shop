3cc4b711a955d42dcfc3fbcd1f48f31382170f05
Samuel Morais030202 <moraissamuel009@gmail.com>
2025-01-26 10:51:53 -0300

    feat: criando paǵina de cadastro

---
+++ src/pages/auth/sign-up.tsx
@@ -0,0 +1,108 @@
+import { zodResolver } from '@hookform/resolvers/zod'
+import { Helmet } from 'react-helmet-async'
+import { useForm } from 'react-hook-form'
+import { Link, useNavigate } from 'react-router-dom'
+import { toast } from 'sonner'
+import { z } from 'zod'
+
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+
+const signInFormSchema = z.object({
+  email: z.string().email(),
+  restaurantName: z.string(),
+  managerName: z.string(),
+  phone: z.string(),
+})
+
+type SignUpForm = z.infer<typeof signInFormSchema>
+
+export function SiginUp() {
+  const navigate = useNavigate()
+
+  const { register, handleSubmit } = useForm<SignUpForm>({
+    resolver: zodResolver(signInFormSchema),
+  })
+
+  function handleSignIn(data: SignUpForm) {
+    console.log(data)
+    toast.success('Restaurante cadastrado com sucesso!', {
+      action: {
+        label: 'Login',
+        onClick: () => navigate('/sign-in'),
+      },
+    })
+  }
+
+  return (
+    <div>
+      <Helmet title="Cadastro" />
+      <div className="p-8">
+        <Button variant="ghost" asChild className="absolute right-8 top-8">
+          <Link to="/sign-in" className="">
+            Fazer Login
+          </Link>
+        </Button>
+
+        <div className="flex w-[350px] flex-col justify-center gap-6">
+          <div className="flex flex-col gap-2 text-center">
+            <h1 className="text-2xl font-semibold tracking-tighter">
+              Criar conta grátis
+            </h1>
+            <p className="text-sm text-muted-foreground">
+              Seja um parceiro e comece suas vendas!
+            </p>
+          </div>
+
+          <form
+            className="flex flex-col gap-4"
+            onSubmit={handleSubmit(handleSignIn)}
+          >
+            <div className="space-y-2">
+              <Label htmlFor="restaurantName">Nome do estabelecimento</Label>
+              <Input
+                id="restaurantName"
+                type="text"
+                {...register('restaurantName')}
+              />
+            </div>
+
+            <div className="space-y-2">
+              <Label htmlFor="restaurantName">Seu nome</Label>
+              <Input
+                id="restaurantName"
+                type="text"
+                {...register('managerName')}
+              />
+            </div>
+
+            <div className="space-y-2">
+              <Label htmlFor="email">Seu e-email</Label>
+              <Input id="email" type="email" {...register('email')} />
+            </div>
+
+            <div className="space-y-2">
+              <Label htmlFor="phone">Seu celular</Label>
+              <Input id="phone" type="tel" {...register('phone')} />
+            </div>
+
+            <Button className="w-full">Finalizar cadastro</Button>
+
+            <p className="px-6 text-center text-sm leading-relaxed text-muted-foreground">
+              Ao continuar você concorda com nossos{' '}
+              <a className="underline underline-offset-4" href="#">
+                termos de serviço
+              </a>{' '}
+              e{' '}
+              <a className="underline underline-offset-4" href="#">
+                políticas de privacidade
+              </a>
+              .
+            </p>
+          </form>
+        </div>
+      </div>
+    </div>
+  )
+}
--- src/routes.tsx
+++ src/routes.tsx
@@ -4,6 +4,7 @@ import { AppLayout } from './pages/_layouts/app'
 import { AuthLayout } from './pages/_layouts/auth'
 import { Dashboard } from './pages/app/dashboard'
 import { SiginIn } from './pages/auth/sign-in'
+import { SiginUp } from './pages/auth/sign-up'
 
 export const router = createBrowserRouter([
   {
@@ -14,6 +15,9 @@ export const router = createBrowserRouter([
   {
     path: '/',
     element: <AuthLayout />,
-    children: [{ path: '/sign-in', element: <SiginIn /> }],
+    children: [
+      { path: '/sign-in', element: <SiginIn /> },
+      { path: '/sign-up', element: <SiginUp /> },
+    ],
   },
 ])
