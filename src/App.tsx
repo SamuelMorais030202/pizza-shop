e238880f572a900821e49e6ced942b3c66ea150f
Samuel Morais030202 <moraissamuel009@gmail.com>
2025-01-25 23:19:36 -0300

    feat: configurando tailwind e shadcn

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+export function App() {
+
+  return (
+    <>
+      <div className="h-screen w-full flex justify-center items-center">
+        <Button className="bg-blue-500">Inicio</Button>
+      </div>
+    </>
+  )
+}
