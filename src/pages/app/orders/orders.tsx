b8cd519c57339fc168403bd612d2391763be6a33
Samuel Morais030202 <moraissamuel009@gmail.com>
2025-01-31 14:33:27 -0300

    feat: adicionando componente de paǵinação

--- src/pages/app/orders/orders.tsx
+++ src/pages/app/orders/orders.tsx
@@ -1,5 +1,6 @@
 import { Helmet } from 'react-helmet-async'
 
+import { Pagination } from '@/components/pagination'
 import {
   Table,
   TableBody,
@@ -17,28 +18,31 @@ export function Orders() {
       <Helmet title="Pedidos" />
       <div className="flex flex-col gap-4">
         <h1 className="text-3xl font-bold tracking-tight">Pedidos</h1>
-      </div>
-      <div className="space-y-2.5">
-        <OrderTableFilters />
-        <div className="rounded-md border">
-          <Table>
-            <TableHeader>
-              <TableRow>
-                <TableHead className="w-16"></TableHead>
-                <TableHead className="w-36">Identificador</TableHead>
-                <TableHead className="w-36">Realizado há</TableHead>
-                <TableHead className="w-36">Status</TableHead>
-                <TableHead className="">Cliente</TableHead>
-                <TableHead className="w-36">Total do pedido</TableHead>
-                <TableHead className="w-36"></TableHead>
-                <TableHead className="w-36"></TableHead>
-              </TableRow>
-            </TableHeader>
 
-            <TableBody>
-              <OrderTableRow />
-            </TableBody>
-          </Table>
+        <div className="space-y-2.5">
+          <OrderTableFilters />
+          <div className="rounded-md border">
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead className="w-16"></TableHead>
+                  <TableHead className="w-36">Identificador</TableHead>
+                  <TableHead className="w-36">Realizado há</TableHead>
+                  <TableHead className="w-36">Status</TableHead>
+                  <TableHead className="">Cliente</TableHead>
+                  <TableHead className="w-36">Total do pedido</TableHead>
+                  <TableHead className="w-36"></TableHead>
+                  <TableHead className="w-36"></TableHead>
+                </TableRow>
+              </TableHeader>
+
+              <TableBody>
+                <OrderTableRow />
+              </TableBody>
+            </Table>
+          </div>
+
+          <Pagination pageIndex={0} perPage={10} totalCount={105} />
         </div>
       </div>
     </>
