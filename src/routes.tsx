0dc25b716056faef3f8cc4b4d763e86bf7277bc5
Samuel Morais030202 <moraissamuel009@gmail.com>
2025-03-12 10:44:45 -0300

    feat: criando página de erro

--- src/routes.tsx
+++ src/routes.tsx
@@ -7,12 +7,13 @@ import { Dashboard } from './pages/app/dashboard/dashboard'
 import { Orders } from './pages/app/orders/orders'
 import { SiginIn } from './pages/auth/sign-in'
 import { SiginUp } from './pages/auth/sign-up'
+import { Error } from './pages/error'
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <AppLayout />,
-    errorElement: <NotFound />,
+    errorElement: <Error />,
     children: [
       { path: '/', element: <Dashboard /> },
       { path: '/orders', element: <Orders /> },
@@ -26,4 +27,8 @@ export const router = createBrowserRouter([
       { path: '/sign-up', element: <SiginUp /> },
     ],
   },
+  {
+    path: '*',
+    element: <NotFound />
+  }
 ])
