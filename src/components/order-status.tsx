1b66006f2dbdca3ac7a859b921eb702c94530545
Samuel Morais030202 <moraissamuel009@gmail.com>
2025-03-12 15:07:42 -0300

    feat: adicionando testes para componente de OrderStatus

--- src/components/order-status.tsx
+++ src/components/order-status.tsx
@@ -12,7 +12,7 @@ interface IOrderStatusProps {
 const orderStatusMap: Record<OrderStatus, string> = {
   pending: 'Pendente',
   canceled: 'Cancelado',
-  delivered: 'Entrege',
+  delivered: 'Entregue',
   delivering: 'Em entrega',
   processing: 'Em preparo',
 }
@@ -21,19 +21,19 @@ export function OrderStatus({ status }: IOrderStatusProps) {
   return (
     <div className="flex items-center gap-2">
       {status === 'pending' && (
-        <span className="size-2 rounded-full bg-slate-400" />
+        <span className="size-2 rounded-full bg-slate-400" data-testid="badge" />
       )}
 
       {status === 'canceled' && (
-        <span className="size-2 rounded-full bg-rose-500" />
+        <span className="size-2 rounded-full bg-rose-500" data-testid="badge" />
       )}
 
       {status === 'delivered' && (
-        <span className="size-2 rounded-full bg-emerald-500" />
+        <span className="size-2 rounded-full bg-emerald-500" data-testid="badge" />
       )}
 
       {['processing', 'delivering'].includes(status) && (
-        <span className="size-2 rounded-full bg-amber-500" />
+        <span className="size-2 rounded-full bg-amber-500" data-testid="badge" />
       )}
 
       <span className="font-medium text-muted-foreground">
