8f5396d71ea18fd27447be8a030586085a82763e
Samuel Morais030202 <moraissamuel009@gmail.com>
2025-03-13 07:43:28 -0300

    feat: configurando o setup do msw

--- src/env.ts
+++ src/env.ts
@@ -2,6 +2,7 @@ import { z } from 'zod'
 
 const envSchema = z.object({
   VITE_API_URL: z.string().url(),
+  MODE: z.enum(['producion', 'development', 'test'])
 })
 
 export const env = envSchema.parse(import.meta.env)
